00d627bc187c97bc43973c842563282e91d3d738
Caleb Cox <calebcox314@gmail.com>
2016-03-20 18:23:01 -0500

    Assocate emails with a domain

--- es6/app.js
+++ es6/app.js
@@ -13,9 +13,26 @@ const app = express();
 
 // Generate a new random email address
 app.get('/generate', (req, res, next) => {
-  const newEmail = new Email({ address: `${shortid.generate()}@${configuration.domain}` });
-  newEmail.save().then(() => {
-    res.send(newEmail.address);
+  const { domain } = req.query;
+  if (!domain) {
+    next(new Error('No domain provided'));
+  }
+
+  // Try to find an existing email address associated with this domain
+  Email.find({ domain }).then(emails => {
+    if (emails.length > 0) {
+      // Use the found email address
+      return emails[0];
+    }
+
+    // Generate a new email address
+    const newEmail = new Email({
+      address: `${shortid.generate()}@${configuration.domain}`,
+      domain,
+    });
+    return newEmail.save();
+  }).then(email => {
+    res.send(email.address);
   }).catch(err => next(err));
 });
 
--- es6/models.js
+++ es6/models.js
@@ -5,6 +5,7 @@ const { Schema } = mongoose;
 // Create the Mongoose models
 const Email = mongoose.model('email', new Schema({
   address: { type: String, required: true },
+  domain: { type: String, required: true },
 }));
 
 export default {
